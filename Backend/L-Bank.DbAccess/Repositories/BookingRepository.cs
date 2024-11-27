34be2432aa318f9b9da99e920bf0b56554d8c7d3
Yusuf Sabir <yusuf.sabir@swisscom.com>
2024-11-27 14:57:44 +0100

    Deleted LBank CLI

--- Backend/L-Bank.DbAccess/Repositories/BookingRepository.cs
+++ Backend/L-Bank.DbAccess/Repositories/BookingRepository.cs
@@ -1,4 +1,3 @@
-﻿using L_Bank_W_Backend.DbAccess.Data;
 using L_Bank_W_Backend.Models;
 using Microsoft.Extensions.Options;
 
@@ -6,26 +5,77 @@ namespace L_Bank_W_Backend.DbAccess.Repositories
 {
     public class BookingRepository : IBookingRepository
     {
-        DatabaseSettings settings;
-        public BookingRepository(IOptions<DatabaseSettings> settings)
-        {
-            this.settings = settings.Value;
-        }
+        // private readonly DatabaseSettings databaseSettings;
+        // public BookingRepository(IOptions<DatabaseSettings> databaseSettings)
+        // {
+        //     this.databaseSettings = databaseSettings.Value;
+        // }
         
-        public bool Book(int sourceLedgerId, int destinationLKedgerId, decimal amount)
-        {
-            // Machen Sie eine Connection und eine Transaktion
-
-            // In der Transaktion:
-
-            // Schauen Sie ob genügend Geld beim Spender da ist
-            // Führen Sie die Buchung durch und UPDATEn Sie die ledgers
-            // Beenden Sie die Transaktion
-            // Bei einem Transaktionsproblem: Restarten Sie die Transaktion in einer Schleife 
-            // (Siehe LedgersModel.SelectOne)
-
-            return false; // Lösch mich
-        }
+        // public bool Book(int sourceLedgerId, int destinationLedgerId, decimal amount)
+        // {
+        //     
+        //     // using (SqlConnection conn = new SqlConnection(this.databaseSettings.ConnectionString))
+        //     // {
+        //     //     conn.Open();
+        //     //     using (SqlTransaction transaction = conn.BeginTransaction(IsolationLevel.Serializable))
+        //     //     {
+        //     //         try
+        //     //         {
+        //     //             amount = 10;
+        //     //         }
+        //     //     }
+        //     // }
+        //     // // Machen Sie eine Connection und eine Transaktion
+        //     // //
+        //     // // In der Transaktion:
+        //     // //
+        //     // // Schauen Sie ob genügend Geld beim Spender da ist
+        //     // // Führen Sie die Buchung durch und UPDATEn Sie die ledgers
+        //     // // Beenden Sie die Transaktion
+        //     // // Bei einem Transaktionsproblem: Restarten Sie die Transaktion in einer Schleife 
+        //     // // (Siehe LedgersModel.SelectOne)
+        //     //
+        //     // return false;
+        // }
+        
+        // public string Book(decimal amount, Ledger from, Ledger to)
+        // {
+        //     // try
+        //     // {
+        //     //     amount = 10;
+        //     //     from.Balance = this.GetBalance(from.Id, conn, transaction) ?? throw new ArgumentNullException();
+        //     //     from.Balance -= amount;
+        //     //     this.Update(from, conn, transaction);
+        //     //     // Complicate calculations
+        //     //     Thread.Sleep(250);
+        //     //     to.Balance = this.GetBalance(to.Id, conn, transaction) ?? throw new ArgumentNullException();
+        //     //     to.Balance += amount;
+        //     //     this.Update(to, conn, transaction);
+        //     //
+        //     //     // Console.WriteLine($"Booking {amount} from {from.Name} to {to.Name}");
+        //     //
+        //     //     transaction.Commit();
+        //     //     return ".";
+        //     // }
+        //     // catch (Exception ex)
+        //     // {
+        //     //     //Console.WriteLine("Commit Exception Type: {0}", ex.GetType());
+        //     //     //Console.WriteLine("  Message: {0}", ex.Message);
+        //     //
+        //     //     // Attempt to roll back the transaction.
+        //     //     try
+        //     //     {
+        //     //         transaction.Rollback();
+        //     //         return "R";
+        //     //     }
+        //     //     catch (Exception ex2)
+        //     //     {
+        //     //         // Handle any errors that may have occurred on the server that would cause the rollback to fail.
+        //     //         //Console.WriteLine("Rollback Exception Type: {0}", ex2.GetType());
+        //     //         //Console.WriteLine("  Message: {0}", ex2.Message);
+        //     //         return "E";
+        //     //     }
+        //     }
     }
 }
 
--- Backend/L-Bank.DbAccess/Repositories/IBookingRepository.cs
+++ Backend/L-Bank.DbAccess/Repositories/IBookingRepository.cs
@@ -2,5 +2,5 @@ namespace L_Bank_W_Backend.Models;
 
 public interface IBookingRepository
 {
-    bool Book(int sourceLedgerId, int destinationLKedgerId, decimal amount);
+    // bool Book(int sourceLedgerId, int destinationLedgerId, decimal amount);
 }
